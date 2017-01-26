94f81fb774030f0e96bfc890030a4c100d2a6f9b
Vora <harshitv@netapp.com>
2017-01-26 14:53:57 -0800

    intial checkin

--- script.js
+++ script.js
@@ -8,26 +8,45 @@ function domobj(){
         for(i=0; i<response.sales.length ; i++){
           self.products.push( new productobj(response.sales[i], i)  );
         }
+        self.updateproducthtml();
     });
   }
+
     
   self.updateproducthtml = function(){
-    for( i=0; i< self.products.length ; i++){
-      self.products[i].updatehtml();
+      $.get('product-template.html', function(template){
+          
+       for( i=0; i< self.products.length ; i++){
+      self.products[i].updatehtml(template);
     }
+          self.updatedom();
+          
+      });
+    
+    
   }
   
   self.updatedom = function(){
+     
     var i=0
     thishtml='';
+      
     for( i=0; i< self.products.length ; i++){
-      if (i % 3 == 0 ){  thishtml += "<div class='row'>"; console.log("START") }
+      if (i % 3 == 0 ){  thishtml += "<div> "; console.log("START") }
       thishtml += self.products[i].htmlview;
       if ((i % 3 == 2) || i == (self.products.length-1) ){thishtml += "</div>";console.log("FINISH")}
+    
     }
     $("#content").append(thishtml)
+    //hiding loading screen once dom is rendered
+    $('#loading').hide();
   }
   
+}
+
+function closeDiv(id){
+   
+    $(id).hide('slow', function(){ $(id).remove(); });
 }
 
 function productobj(product, i){
@@ -36,19 +55,22 @@ function productobj(product, i){
   self.title        = product.name
   self.tagline      = product.tagline
   self.url          = product.url
+  self.description  = product.description
   self.htmlview     = ""
   self.index        = i
-  self.custom_class = "col"+ ((i % 3) +1)
+  self.custom_class = ""
   
-  self.updatehtml= function(){
-    $.get('product-template.html', function(template){
-      self.htmlview = template.replace('{image}', self.photo).replace('{title}', self.title).replace('{tagline}', self.tagline).replace('{url}', self.url).replace('{custom_class}', self.custom_class);
-    });
+  self.updatehtml= function(template){
+  
+      self.htmlview = template.replace('{image}', self.photo).replace('{title}', self.title).replace('{tagline}', self.tagline).replace('{url}', self.url).replace('{custom_class}', self.custom_class).replace('{Description}',self.description).replace('{id}',self.index).replace('{id}',self.index);
+   // });
+      
   }
 }
 
 
 var page=new domobj();
 page.getproducts('data.json');
-setTimeout("console.log('building html');page.updateproducthtml();",20);
-setTimeout("page.updatedom()",50)
+
+//setTimeout("console.log('building html');page.updateproducthtml();",10);
+//setTimeout("page.updatedom()",20)
